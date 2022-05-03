fdc1b405cd5f21b97f0a0396140cde27817c6b8a
parksontyc <parkson.tyc.chen@gmail.com>
2022-05-03 21:32:34 +0800

    創建井字版

---
+++ main.py
@@ -0,0 +1,23 @@
+# 井字遊戲
+"""
+1.井字版
+2.使用者輸入數字1~9來代表位置, 另外注意, 如果位置已經有了輸入，要求使用者重新輸入。
+3.確認贏家。水平垂直及對角線,如果符號相同，就贏。
+"""
+
+# 創建井字版
+
+board = [
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+]
+# print(board)
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(slot, end=' ')
+        print()                    #換行用
+
+print_board(board)
