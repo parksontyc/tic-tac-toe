43274c847a6bfcca270fcdb45098449f1bb5af76
parksontyc <parkson.tyc.chen@gmail.com>
2022-05-04 00:05:23 +0800

    check user input

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 
 # 創建井字版
 
+from re import T
+
+
 board = [
     ['_', '_', '_'],
     ['_', '_', '_'],
@@ -22,12 +25,41 @@ def print_board(board):
 print_board(board)
 
 def quit(user_input):
-    if user_input == "q":
+    if user_input.lower() == "q":
+        print("Thanks for playing ! ")
         return True
     else:
         return False
+
+def check_input(user_input):
+    # ckeck if its a number
+    if not isnum(user_input):
+        print("This is not a valid number !")
+        return False
+    user_input = int(user_input)
+    if not bounds(user_input):
+        return False
+    return True
+
+    #check if its  1-9
+
+def isnum(user_input):
+    if not user_input.isnumeric():
+        return False
+    else:
+        return True
+
+def bounds(user_input):
+    if user_input > 9 or user_input < 1 :
+        print("this is number is out of bounds !")
+        return False
+    else:
+        return True
 
 while True:
     user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit :")
     if quit(user_input) :
         break
+    if not check_input(user_input):
+        print("Please try again !")
+        continue
