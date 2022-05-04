7aff5b69793ee81b8e5cf0b1d5a2235bd2c97b8b
parksontyc <parkson.tyc.chen@gmail.com>
2022-05-04 22:15:23 +0800

    0504

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 # 創建井字版
 
+from itertools import count
 from re import T
 
 
@@ -22,7 +23,7 @@ def print_board(board):
             print(f'{slot}  ', end='')
         print()                        #換行用
 
-print_board(board)
+user = True
 
 def quit(user_input):
     if user_input.lower() == "q":
@@ -56,10 +57,47 @@ def bounds(user_input):
     else:
         return True
 
+def coordinates(user_input):
+    row = int(user_input / 3)
+    col = user_input
+    if col > 2:
+        col = int(user_input % 3)
+    return (row, col)
+
+def istaken(coords, board):
+    row = coords[0]
+    col = coords[1]
+    if board[row][col] != "_":
+        print("this position is already taken.")
+        return True
+    else:
+        return False
+
+def add_to_board(coords, board, active_user):
+    row = coords[0]
+    col = coords[1]
+    board[row][col] = active_user
+
+def current_user(user):
+    if user:
+        return "x"
+    else:
+        return "o"
+
 while True:
+    active_user = current_user(user)
+    print_board(board)
     user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit :")
     if quit(user_input) :
         break
     if not check_input(user_input):
         print("Please try again !")
         continue
+    user_input = int(user_input) - 1
+    coords = coordinates(user_input)
+    if istaken(coords, board):
+        print("Please try again !")
+        continue
+    add_to_board(coords, board, active_user)
+    user = not user
+    
