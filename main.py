432da6cf14f2a5a6d6af665e7da8252483e16f3b
parksontyc <parkson.tyc.chen@gmail.com>
2022-05-05 22:31:59 +0800

    command

--- main.py
+++ main.py
@@ -25,46 +25,45 @@ def print_board(board):
 
 user = True
 
-def quit(user_input):
+def quit(user_input):  # 離開遊戲
     if user_input.lower() == "q":
         print("Thanks for playing ! ")
         return True
     else:
         return False
 
-def check_input(user_input):
+def check_input(user_input):  # 確認使用者輸入資訊是否為數字或是否超過1~9
     # ckeck if its a number
     if not isnum(user_input):
         print("This is not a valid number !")
         return False
     user_input = int(user_input)
+    #check if its  1-9
     if not bounds(user_input):
         return False
     return True
 
-    #check if its  1-9
-
-def isnum(user_input):
+def isnum(user_input):  # 確認輸入是否為數字
     if not user_input.isnumeric():
         return False
     else:
         return True
 
-def bounds(user_input):
+def bounds(user_input): # 確認輸入是否超過1~9這個範圍
     if user_input > 9 or user_input < 1 :
         print("this is number is out of bounds !")
         return False
     else:
         return True
 
-def coordinates(user_input):
+def coordinates(user_input): # 將使用輸入的數字，轉化為board的座標
     row = int(user_input / 3)
     col = user_input
     if col > 2:
         col = int(user_input % 3)
     return (row, col)
 
-def istaken(coords, board):
+def istaken(coords, board): # 確認位置是否已有值
     row = coords[0]
     col = coords[1]
     if board[row][col] != "_":
@@ -73,12 +72,12 @@ def istaken(coords, board):
     else:
         return False
 
-def add_to_board(coords, board, active_user):
+def add_to_board(coords, board, active_user): # 將xo放入清單中
     row = coords[0]
     col = coords[1]
     board[row][col] = active_user
 
-def current_user(user):
+def current_user(user): # ox交換
     if user:
         return "x"
     else:
